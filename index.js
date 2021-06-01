e9bea6a60d26e5c466043eade7739b41fe1977e9
yoshinorin <yoshinorin.net@outlook.com>
2021-06-02 01:09:17 +0900

    render codeblock correctly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const pkg = require('./package.json');
 
-hexo.extend.tag.register('insertmd', function(args){
+hexo.extend.tag.register('insertmd', async (args) => {
   const [filePath] = args;
   const file = path.join(hexo.source_dir, filePath);
 
@@ -10,7 +10,6 @@ hexo.extend.tag.register('insertmd', function(args){
     throw new Error(`[${pkg.name}] File "${file}" does not exist.`);
   }
 
-  return hexo.render.renderSync({
-    path: file
-  });
-});
+  const result = await hexo.post.render(source = file)
+  return result.content;
+}, {async: true});
