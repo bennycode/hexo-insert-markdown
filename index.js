9cb4efe4f325abbdf7e0b9f78601d9b2f1339dba
Benny Neugebauer <bn@bennyn.de>
2021-07-11 17:25:07 +0200

    feat: Render multiple files from directory (resolves #2)

--- index.js
+++ index.js
@@ -5,15 +5,35 @@ const pkg = require('./package.json');
 hexo.extend.tag.register(
   'insertmd',
   async args => {
+    const renderSeparator = separator => {
+      return separator ? hexo.render.renderSync({text: separator, engine: 'md'}) : '';
+    };
+
+    const renderFile = async (file, separator) => {
+      const {content} = await hexo.post.render(file);
+      return `${content}${renderSeparator(separator)}`;
+    };
+
     const [filePath, separator] = args;
     const file = path.join(hexo.source_dir, filePath);
 
     if (!fs.existsSync(file)) {
-      throw new Error(`[${pkg.name}] File "${file}" does not exist.`);
+      throw new Error(`[${pkg.name}] Path "${file}" does not exist.`);
     }
 
-    const {content} = await hexo.post.render(file);
-    return separator ? content + hexo.render.renderSync({text: separator, engine: 'md'}) : content;
+    const isDirectory = fs.lstatSync(file).isDirectory();
+
+    if (isDirectory) {
+      const promises = fs
+        .readdirSync(file)
+        .map(markDownFile => `${file}${path.sep}${markDownFile}`)
+        .filter(filePath => path.extname(filePath) === '.md')
+        .map(filePath => renderFile(filePath));
+      const contents = await Promise.all(promises);
+      return contents.join(renderSeparator(separator));
+    } else {
+      return renderFile(file, separator);
+    }
   },
   {async: true}
 );
